a292d3ca78a240424b43ac433bfdd6b5db49141d
Mohammed Abdelsalhyen <114920462+mohammedsalhyen@users.noreply.github.com>
2025-09-02 20:19:41 +0300

    Update data.ts

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -3,7 +3,7 @@ import { Experience } from '../types/index';
 import { ProjectType } from '../types/index';
 import { SkillType } from '../types/index';
 
-const API_BASE_URL = 'https://portfolio-backend-a561.onrender.com';
+const API_BASE_URL = 'https://portfolio-backend-production-bc7a.up.railway.app';
 
 async function fetchData<T>(endpoint: string): Promise<T> {
     const res = await fetch(`${API_BASE_URL}/${endpoint}`);
